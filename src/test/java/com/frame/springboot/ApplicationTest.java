e147d0f5abd309e54014131408a119ced613b45e
yj1048883467@126.com <19941130>
2018-09-20 09:47:05 +0800

    添加了新的文件

---
+++ src/test/java/com/frame/springboot/ApplicationTest.java
@@ -0,0 +1,27 @@
+package com.frame.springboot;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTest implements CommandLineRunner {
+
+
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication app = new SpringApplication(ApplicationTest.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+
+
+    }
+
+}
