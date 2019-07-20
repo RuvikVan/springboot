153aa6b9e72378cc55c8c8cadde07a974a0484b6
yj1048883467@126.com <Yj19941130>
2019-07-20 21:37:31 +0800

    提交了的测试

--- src/test/java/com/frame/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/frame/springboot/SpringbootApplicationTests.java
@@ -1,17 +1,17 @@
-package com.frame.springboot;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class SpringbootApplicationTests {
-
-	@Test
-	public void contextLoads() {
-		System.out.println("qwe");
-	}
-
-}
+//package com.frame.springboot;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class SpringbootApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//		System.out.println("qwe");
+//	}
+//
+//}
